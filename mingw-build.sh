a312ea48a651f5781ed7139778756f9033e201e5
PChemGuy <39730837+pchemguy@users.noreply.github.com>
2021-05-05 01:37:05 +0300

    Build draft

--- mingw-build.sh
+++ mingw-build.sh
@@ -27,173 +27,49 @@ readonly DBDIR="sqlite3"
 readonly BUILDDIR=${DBDIR}/build
 
 
-get_sqlite() {
-  cd "${BASEDIR}" || ( echo "Cannot enter ${BASEDIR}" && exit 101 )
-  local SQLite_URL="https://www.sqlite.org/src/tarball/sqlite.tar.gz?r=release"
-  if [[ ! -f ./sqlite.tar.gz ]]; then
-    echo "____________________________________________"
-  	echo "Downloading the current release of SQLite..."
-    echo "--------------------------------------------"
-    wget -c "${SQLite_URL}" --no-check-certificate -O sqlite.tar.gz \
-      || EC=$?
-    (( EC != 0 )) && echo "Error downloading SQLite ${EC}." && exit 102
+setenv_sqliteodbc() {
+  SQLITE_DLLS=${SQLITE_DLLS:-}
+  ADD_NSIS=${ADD_NSIS:-}
+
+  if [[ "${SQLITE_DLLS}" == "2" ]]; then
+    # turn on -DSQLITE_DYNLOAD in sqlite3odbc.c
+    export ADD_CFLAGS="-DWITHOUT_SHELL=1 -DWITH_SQLITE_DLLS=2"
+    ADD_NSIS="${ADD_NSIS} -DWITHOUT_SQLITE3_EXE"
+  elif [[ -n "${SQLITE_DLLS}" ]]; then
+    export ADD_CFLAGS="-DWITHOUT_SHELL=1 -DWITH_SQLITE_DLLS=1"
+    export SQLITE3_DLL="-Lsqlite3 -lsqlite3"
+    ADD_NSIS="${ADD_NSIS} -DWITHOUT_SQLITE3_EXE -DWITH_SQLITE_DLLS"
   else
-    echo "________________________________________________"
-  	echo "Using previously downloaded archive of SQLite..."
-    echo "------------------------------------------------"
+    export SQLITE3_A10N_O="sqlite3.o"
+    ADD_NSIS="${ADD_NSIS} -DWITHOUT_SQLITE3_EXE"
   fi
 
-  if [[ ! -f "./${DBDIR}/configure" ]]; then
-    tar xzf ./sqlite.tar.gz
-    mv ./sqlite "${DBDIR}"
-  fi
-  return 0
-}
+  export NO_SQLITE2=1
+  export NO_TCCEXT=1
+  export ADD_NSIS
 
-
-configure_sqlite() {
-  mkdir -p "./${BUILDDIR}"
-  cd "${BASEDIR}/${BUILDDIR}" \
-    || ( echo "Cannot enter ./${BUILDDIR}" && exit 104 )
-  [[ ! -r ../configure ]] && echo "Error accessing SQLite configure" && exit 105
-
-  if [[ ! -f ./Makefile ]]; then
-    echo "______________________"
-  	echo "Configuring SQLite3..."
-    echo "----------------------"
-    ../configure --enable-fts3 --enable-memsys5 --enable-update-limit \
-      --enable-all --with-tcl="${MINGW_PREFIX}/lib/tcl8" || EXITCODE=$?
-    (( EXITCODE != 0 )) && echo "Error configuring SQLite" && exit 106
-  else
-    echo "____________________________________________"
-  	echo "Makefile found. Skipping configuring SQLite3"
-    echo "--------------------------------------------"
-  fi
   return 0
 }  
 
 
-patch_sqlite3_makefile() {
-  cd "${BASEDIR}/${BUILDDIR}" \
-    || ( echo "Cannot enter ./${BUILDDIR}" && exit 108 )
-  echo "____________________________"
-  echo "Patching SQLite3 Makefile..."
-  echo "----------------------------"
-  sed -e 's|^CFLAGS =\(.*\)$|CFLAGS :=\1 \$(CFLAGS)|;' \
-      -e 's|^OPT_FEATURE_FLAGS =\(.*\)$|OPT_FEATURE_FLAGS :=\1 \$(OPT_FEATURE_FLAGS)|;' \
-      -e "s|^TOP = \(.*\)$|TOP = ${BASEDIR}/${DBDIR}|;" \
-      -i Makefile
-  return 0
-}
-
-
-sys_lib_path=""
-libtool_sys_lib_path() {
-  cd "${BASEDIR}/${BUILDDIR}" \
-    || ( echo "Cannot enter ./${BUILDDIR}" && exit 107 )
-  echo "_____________________________________"
-  echo "Cleaning up libtool's sys_lib_path..."
-  echo "-------------------------------------"
-  local msys_root
-  msys_root="$(cygpath -m /)"
-  msys_root="${msys_root%/}"
-  local folders
-  IFS=$' \n\t'
-  read -r -a folders <<< \
-    "$(grep -oP '(?<=^sys_lib_search_path_spec=" =)(.*)(?="$)' ./libtool)"
-  IFS=$'\n\t'
-  sys_lib_path=":"
-  local folder
-  for folder in "${folders[@]}"; do
-    folder="$(realpath "${folder}" || true)"
-    if [[ -n "${folder}" ]]; then
-      folder="${folder#${msys_root}}"
-      if [[ -n "${sys_lib_path##*${folder}:*}" ]]; then
-        sys_lib_path+="${folder}:"
-      fi
-    fi
-  done
-
-  sys_lib_path="${sys_lib_path%:}"
-  sys_lib_path="${sys_lib_path#:}"
-  sys_lib_path="${sys_lib_path//:/ }"
-  return 0
-}
-
-
-patch_sqlite3_libtool() {
-  cd "${BASEDIR}/${BUILDDIR}" \
-    || ( echo "Cannot enter ./${BUILDDIR}" && exit 108 )
-  echo "___________________________"
-  echo "Patching SQLite3 libtool..."
-  echo "---------------------------"
-  local mingw_ld="$(which ld.exe)"
-  sed -e 's|^\(deplibs_check_method=\)"file_magic\(.*\)$|#\0\n\1"pass_all"|;' \
-      -e "s|^LD=\(.*\)\$|LD=\"${mingw_ld}\"|;" \
-      -e "s|^\(sys_lib_search_path_spec=\)\(.*\)\$|\1\"${sys_lib_path}\"|;" \
-      -i libtool
-  return 0
-}
-
-
-gen_sqlite3_amalgamation() {
-  echo "__________________________________"
-  echo "Generating SQLite3 amalgamation..."
-  echo "----------------------------------"
-  make -C "${BASEDIR}/${BUILDDIR}" sqlite3.c || EXITCODE=$?
-  (( EXITCODE != 0 )) && echo "Error generating SQLite3 amalgamation" && exit 109
-  return 0
-}
-
-
-patch_sqlite3_libshell_c() {
-  echo "__________________________________________________________________"
-  echo "Adjust names of the entry point and appendText in (lib)shell.c ..."
-  echo "------------------------------------------------------------------"  
-  cp "${BASEDIR}/${BUILDDIR}/shell.c" "${BASEDIR}/${BUILDDIR}/libshell.c" || EXITCODE=$?
-  (( EXITCODE != 0 )) && echo "Error copying <shell.c>." && exit 110
-  sed -e 's/^int SQLITE_CDECL main/int SQLITE_CDECL sqlite3_main/;' \
-      -e 's/appendText/shAppendText/g;' \
-      -i "${BASEDIR}/${BUILDDIR}/libshell.c"
-  return 0
-}
-
-
 main() {
-  export NO_SQLITE2=1
-  export NO_TCCEXT=1
-
   LOG_FILE=${LOG_FILE:-makelog.log}
   echo "SQLITE_DLLS=${SQLITE_DLLS:-};" "$0" "$@" >>"${LOG_FILE}"
   echo "###############################################################" >>"${LOG_FILE}"
   echo "" >>"${LOG_FILE}"
 
-  get_sqlite || EXITCODE=$?
-  (( EXITCODE != 0 )) && echo "Error downloading SQLite3" && exit 201
-  configure_sqlite || EXITCODE=$?
-  (( EXITCODE != 0 )) && echo "Error configuring SQLite3" && exit 202
-  patch_sqlite3_makefile || EXITCODE=$?
-  if [[ -n "${MINGW_PREFIX:-}" ]]; then
-    echo "___________________________________"
-    echo "MINGW detected. Patching libtool..."
-    echo "-----------------------------------"
-    libtool_sys_lib_path || EXITCODE=$?
-    patch_sqlite3_libtool || EXITCODE=$?
-    (( EXITCODE != 0 )) && echo "Error patching <libtool>" && exit 203
-  fi
+  "${BASEDIR}/sqlite3.ref.sh" all || EXITCODE=$?
+  (( EXITCODE != 0 )) && echo "Error preparing SQLite3" && exit 201
+  cp "${DBDIR}/build/sqlite3.o" "${BASEDIR}" \
+    || ( echo "Cannot copy SQLite3 binaries" && exit 202 )
+  setenv_sqliteodbc || EXITCODE=$?
+  (( EXITCODE != 0 )) && echo "Error setting env for SQLiteODBC" && exit 201
 
-  gen_sqlite3_amalgamation || EXITCODE=$?
-  [[ ! -r "${BASEDIR}/${BUILDDIR}/shell.c" \
-  || ! -r "${BASEDIR}/${BUILDDIR}/sqlite3.c" \
-  || ! -r "${BASEDIR}/${BUILDDIR}/sqlite3.h" ]] \
-    && echo "Error creating SQLite3 amalgamation." && exit 204
+  echo "================================================"
+  echo "Building ODBC drivers, utils, and NSIS installer"
+  echo "================================================"
+  make -C "${BASEDIR}" -f Makefile_d.mingw all
 
-  exit
-
-  patch_sqlite3_libshell_c || EXITCODE=$?
-  (( EXITCODE != 0 )) && echo "Error patching <libshell.c>" && exit 205
-
-  make -C "${BASEDIR}" -f "mf-sqlite3.mingw" sqlite3.o #echo_CCCLI
   return 0
 }
 
